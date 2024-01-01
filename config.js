9a199e63fa65cb276c547b2cf78cb08bb908bcda
Heny Patel <heny09112@gmail.com>
2024-01-01 18:15:34 -0500

    added config

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyByJuER_Kpn9DzXueBvshuQbalvTPsA71o",
+  authDomain: "reminderapp-9a77d.firebaseapp.com",
+  projectId: "reminderapp-9a77d",
+  storageBucket: "reminderapp-9a77d.appspot.com",
+  messagingSenderId: "719751685199",
+  appId: "1:719751685199:web:08d2b1a15a6312c4e3b63a"
+};
